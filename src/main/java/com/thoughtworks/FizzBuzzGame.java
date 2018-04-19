745daa4943a1e8117f94460ba55b5d6095b65f37
niubilityxiesong <563527588@qq.com>
2018-04-19 16:51:16 +0800

    finishi

--- src/main/java/com/thoughtworks/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/FizzBuzzGame.java
@@ -5,13 +5,88 @@ import java.util.List;
 
 
 public class FizzBuzzGame {
-    private List<String> results = new ArrayList<>();
+    public List<String> results = new ArrayList<>();
+    public List<String> allnumber = new ArrayList<>();
+    public int keywords = -1;
+    String array3 = "Fizz";
+    String array5 = "Buzz";
+    String array7 = "Whizz";
 
     public void start(int count) {
 
+        int i;
+        String temp = new String();
+        for(i = 1; i < count + 1; i++){
+
+            allnumber.clear();
+            IntToString(i);
+            keywords = FindKeyWords();
+            if(keywords > 0){
+                temp = array3;
+                results.add(temp);
+                temp = "";
+                keywords = -1;
+            }
+            else {
+                rule1(i);
+            }
+        }
+
+        getResults();
     }
 
+    public void IntToString(int number){
+        int sum = 1;
+        int tempnumber = number;
+
+        tempnumber /= 10;
+        while(tempnumber > 0){
+            tempnumber /= 10;
+            sum *= 10;
+        }
+
+        while (sum > 0){
+            tempnumber = number / sum;
+            number %= sum;
+            sum /= 10;
+            allnumber.add(String.valueOf(tempnumber));
+        }
+    }
+
+    public int FindKeyWords(){
+
+        for(int i = 0; i < allnumber.size(); i++){
+            if(allnumber.get(i).equals("3")){
+                return 1;
+            }
+        }
+        return -1;
+    }
+
+    public void rule1(int i){
+
+
+        String temp = new String();
+        if(i % 3 == 0){
+            temp += array3;
+        }
+        if(i % 5 == 0){
+            temp += array5;
+        }
+        if(i % 7 == 0){
+            temp += array7;
+        }
+        if(temp.length() == 0) {
+            temp = String.valueOf(i);
+        }
+        results.add(temp);
+
+    }
+
+
     public List<String> getResults() {
+
+
         return results;
     }
 }
--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -1,10 +1,52 @@
 package com.thoughtworks;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 
 public class FizzBuzzGameTest {
     @Test
     public void should_return_Fizz_when_have_3() {
 
+    }
+
+    @Test
+    public void testIntToString() throws Exception {
+        FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+
+        fizzBuzzGame.IntToString(13);
+        assertThat(fizzBuzzGame.allnumber.get(0)).isEqualTo("1");
+        assertThat(fizzBuzzGame.allnumber.get(1)).isEqualTo("3");
+    }
+
+    @Test
+    public void testKeywords() throws Exception {
+
+        FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+
+        fizzBuzzGame.IntToString(13);
+
+        assertThat(fizzBuzzGame.FindKeyWords()).isEqualTo(1);
+    }
+
+    @Test
+    public void testrule1() throws Exception {
+
+        FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+        String result= "FizzBuzz";
+
+        fizzBuzzGame.rule1(15);
+        assertThat(result).isEqualTo(fizzBuzzGame.results.get(0));
+
+    }
+
+    @Test
+    public void testResultSize() throws Exception {
+
+        FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+        fizzBuzzGame.start(100);
+        assertThat(fizzBuzzGame.results.size()).isEqualTo(100);
     }
 }
